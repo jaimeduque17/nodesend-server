f2b46f8cda4b976008ef1a3af5f20671193e1729
Jaime Duque <jaime.duque@estratek.com.co>
2020-07-28 21:30:14 -0500

    route and controller for the user are created

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ connectionDB();
 // app port
 const port = process.env.PORT || 4000;
 
+// routes
+app.use('/api/users', require('./routes/users'));
+
 // run app
 app.listen(port, '0.0.0.0', () => {
     console.log(`The server is running on port ${port}`)
